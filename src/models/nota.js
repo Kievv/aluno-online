1a96f1d01081b0764ba57c125ab2a3c5c54cd136
unknown <2124290038@ACADEMICO.IESB.BR>
2023-05-09 21:20:01 -0300

    commit

--- src/models/nota.js
+++ src/models/nota.js
@@ -8,23 +8,23 @@ class Nota {
 
     mediaFinal() {
         return Math.max(
-            0.4 * this.a1 + 0.6 * this.a2, 
-            0.4 * this.a1 + 0.6 * this.a3, 
-            0.4 * this.a3 + 0.6 * this.a2
+            0.4 * this.a1 + 0.6 * this.a2,
+            0.4 * this.a1 + 0.6 * this.a3,
+            0.4 * this.a3 + 0.6 * this.a2,
         );
     }
 
     mediaCA() {
-        
-        if(this.mediaFinal() < 5.0 && this.mediaFinal() > 1){
+
+        if (this.mediaFinal() < 5.0 && this.mediaFinal() > 1) {
             return 'MI'
-        }else if(this.mediaFinal() < 1){
+        } else if (this.mediaFinal() < 1) {
             return 'SR'
-        }else if(this.mediaFinal() >= 5 && this.mediaFinal() < 7){
+        } else if (this.mediaFinal() >= 5 && this.mediaFinal() < 7) {
             return 'MM'
-        }else if(this.mediaFinal() >=7 && this.mediaFinal() <= 9){
+        } else if (this.mediaFinal() >= 7 && this.mediaFinal() <= 9) {
             return 'MS'
-        }else if(this.mediaFinal() > 9 && this.mediaFinal() <= 10){
+        } else if (this.mediaFinal() > 9 && this.mediaFinal() <= 10) {
             return 'SS'
         }
     }
