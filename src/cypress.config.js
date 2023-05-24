d64634b5bd45b91f8fefedd3fb5fd60325ca0afe
iev <ievsouza622@gmail.com>
2023-05-23 21:47:06 -0300

    testes

---
+++ src/cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  baseURL: "http://127.0.0.1:3000",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
