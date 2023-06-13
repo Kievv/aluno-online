7a9eb71a0887f9e7d4755faf71ac8de7bbcf79b3
iev <ievsouza622@gmail.com>
2023-06-13 20:38:35 -0300

    Não é cola

--- src/models/nota.js
+++ src/models/nota.js
@@ -11,7 +11,7 @@ class Nota {
   }
 
   mediaCA() {
-    if (this.mediaFinal() < 5.0 && this.mediaFinal() > 3) {
+    if (this.mediaFinal() < 5.0 && this.mediaFinal() >= 3) {
       return 'MI';
     } else if (this.mediaFinal() == 0) {
       return 'SR';
@@ -28,3 +28,56 @@ class Nota {
 }
 
 module.exports = Nota;
+
+
+// describe('Adicionando um requerimento de disciplina Manutenção de Software', () => {
+//   it('passes', () => {
+//     cy.visit('localhost:3000/requerimentos');
+
+//     cy.get('.btn')
+//       .click()
+//       .then(() => {
+//         cy.get('#inputDisciplina').select('ADS030 - Manutenção de Software e DevOps');
+//         cy.get('#inputProva').select('A1');
+//         cy.get('#inputArgumentacao').type('Teste de argumentação', { delay: 20 });
+//         cy.get('.btn-danger').click();
+
+//         cy.get(':nth-child(6) > .col-md-5').should('be.visible');
+//       });
+//   });
+// });
+
+// describe('Adicionando um requerimento de disciplina Construção de Frontend ', () => {
+//   it('passes', () => {
+//     cy.visit('localhost:3000/requerimentos');
+
+//     cy.get('.btn')
+//       .click()
+//       .then(() => {
+//         cy.get('#inputDisciplina').select('ADS032 - Construção de Frontend');
+//         cy.get('#inputProva').select('A2');
+//         cy.get('#inputArgumentacao').type('Outro teste de argumentação', { delay: 20 });
+//         cy.get('.btn-danger').click();
+
+//         cy.get('.col-md-5').should('be.visible');
+//       });
+//   });
+// });
+
+// describe('Testando funcionalidade do botão desistir', () => {
+//   it('passes', () => {
+//     cy.visit('localhost:3000/requerimentos');
+
+//     cy.get('.btn')
+//       .click()
+//       .then(() => {
+//         cy.get('#inputDisciplina').select('ADS032 - Construção de Frontend');
+//         cy.get('#inputProva').select('A2');
+//         cy.get('#inputArgumentacao').type('Outro teste de argumentação', { delay: 20 });
+
+//         cy.get('.btn-light').click();
+
+//         cy.url().should('eq', 'http://localhost:3000/requerimentos');
+//       });
+//   });
+// });
